8d7ee189ef63c54a5ecd56f2957bf1166bb329f4
Jan <jan.escorza99@gmail.com>
2019-10-26 11:48:47 +0200

    tets 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //test
+        System.out.println("hi there");
         System.out.println("hello");
     }
 }
