6af7b82b3a38979ce19ee2029e40175ae5a40b0e
sheidavanesa <sheidaat99@gmail.com>
2019-10-26 14:37:06 +0200

    Borrar outs

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //test
-        System.out.println("hi there");
-        System.out.println("hello");
-        System.out.println("hello world");
-        System.out.println("lzw");
-        System.out.println("peme");
-        int hello = 0;
-        System.out.println(hello);
     }
 }
