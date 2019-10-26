509ac0412e9f885b9e527cdc4cd3594403e26632
Jan <jan.escorza99@gmail.com>
2019-10-26 14:23:02 +0200

    test despues de comer

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //test
+        System.out.println("this is a test");
         System.out.println("hi there");
         System.out.println("hello");
         System.out.println("hello world");
