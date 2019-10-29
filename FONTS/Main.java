680ac0772e0d0e91011e4bf33d5725abe7c64f93
Joan Gámez <joan.gamez@est.fib.upc.edu>
2019-10-29 17:03:51 +0100

    improved main and jpeg tweaks

--- FONTS/Main.java
+++ FONTS/Main.java
@@ -18,8 +18,14 @@ public class Main {
             String comando = scanner.nextLine();
             switch (comando) {
                 case "comprimir":
-                    System.out.println("Escriba el path absoluto del fichero a comprimir (.txt o .ppm!)");
-                    File fileInComp = new File(scanner.nextLine());
+                case "1":
+                    String s;
+                    if (args.length == 0) {
+                        System.out.println("Escriba el path absoluto del fichero a comprimir (.txt o .ppm!)");
+                        s = scanner.nextLine();
+                    }
+                    else s = args[0];
+                    File fileInComp = new File(s);
                     if (fileInComp.getName().endsWith(".txt")) {
                         System.out.printf("Escriba el algoritmo de compresión que quiera usar, de entre los siguientes:\npredeterminado\nlzss\nlz78\nlzw\n");
                         String algoritmoComp = scanner.nextLine();
@@ -39,35 +45,41 @@ public class Main {
                             default:
                                 throw new EnumConstantNotPresentException(Algoritmos.class, "El tipo de compresor " + algoritmoComp + " no existe o no está disponible para un archivo .txt\n");
                         }
-
-                            File fileOutComp = ctrlProcesos.comprimirArchivo(fileInComp, tipoCompresor);
-                            if (fileOutComp != null)
-                                System.out.println("El archivo " + fileInComp.getName() + " se ha comprimido correctamente!\n");
+                        File fileOutComp = ctrlProcesos.comprimirArchivo(fileInComp, tipoCompresor);
+                        if (fileOutComp != null)
+                            System.out.println("El archivo " + fileInComp.getName() + " se ha comprimido correctamente!\n");
                     }
                     else if (fileInComp.getName().endsWith(".ppm")) {
-
-                            System.out.println("Indique la calidad de compresión a usar (del 1 al 10)");
-                            ctrlProcesos.setCalidadJPEG(scanner.nextInt() * 10);
-                            File fileOutComp = ctrlProcesos.comprimirArchivo(fileInComp);
-                            if (fileOutComp != null)
-                                System.out.println("El archivo " + fileInComp.getName() + " se ha comprimido correctamente!\n");
-
+                        System.out.println("Indique la calidad de compresión a usar (del 1 al 10)");
+                        ctrlProcesos.setCalidadJPEG(scanner.nextInt() * 10);
+                        File fileOutComp = ctrlProcesos.comprimirArchivo(fileInComp);
+                        if (fileOutComp != null)
+                            System.out.println("El archivo " + fileInComp.getName() + " se ha comprimido correctamente!\n");
                     }
                     else System.out.println("El formato del fichero debe de ser .txt o .ppm!");
                     break;
                 case "descomprimir":
-                    System.out.println("Escriba el path absoluto del fichero a descomprimir (.lzss, .lz78, lzw o .imgc!)");
-                    File fileInDesc = new File(scanner.nextLine());
-                    File fileOutDesc;
-                    try {
-                        fileOutDesc = ctrlProcesos.descomprimirArchivo(fileInDesc);
-                        if(fileOutDesc != null) System.out.println("El archivo se ha descomprimido correctamente!\n");
+                case "2":
+                    String str;
+                    if (args.length == 0) {
+                        System.out.println("Escriba el path absoluto del fichero a descomprimir (.lzss, .lz78, lzw o .imgc!)");
+                        str = scanner.nextLine();
                     }
-                    catch (Exception e) {}
+                    else str = args[0];
+                    File fileInDesc = new File(str);
+                    File fileOutDesc;
+                    fileOutDesc = ctrlProcesos.descomprimirArchivo(fileInDesc);
+                    if(fileOutDesc != null) System.out.println("El archivo se ha descomprimido correctamente!\n");
                     break;
                 case "compdesc":
-                    System.out.println("Escriba el path absoluto del fichero a comprimir (.txt o .ppm!)");
-                    File fileInCompDesc = new File(scanner.nextLine()), fileComp = null, fileOutCompDesc;
+                case "3":
+                    String string;
+                    if (args.length == 0) {
+                        System.out.println("Escriba el path absoluto del fichero a comprimir (.txt o .ppm!)");
+                        string = scanner.nextLine();
+                    }
+                    else string = args[0];
+                    File fileInCompDesc = new File(string), fileComp = null, fileOutCompDesc;
                     if (fileInCompDesc.getName().endsWith(".txt")) {
                         System.out.printf("Escriba el algoritmo de compresión que quiera usar, de entre los siguientes:\npredeterminado\nlzss\nlz78\nlzw\n");
                         String algoritmoComp = scanner.nextLine();
@@ -87,30 +99,23 @@ public class Main {
                             default:
                                 throw new EnumConstantNotPresentException(Algoritmos.class, "El tipo de compresor " + algoritmoComp + " no existe o no está disponible para un archivo .txt\n");
                         }
-                        try {
-                            fileComp = ctrlProcesos.comprimirArchivo(fileInCompDesc, tipoCompresor);
-                            if (fileComp != null)
-                                System.out.println("El archivo " + fileInCompDesc.getName() + " se ha comprimido correctamente!\n");
-                        }
-                        catch (Exception e) {}
+                        fileComp = ctrlProcesos.comprimirArchivo(fileInCompDesc, tipoCompresor);
+                        if (fileComp != null)
+                            System.out.println("El archivo " + fileInCompDesc.getName() + " se ha comprimido correctamente!\n");
                     }
                     else if (fileInCompDesc.getName().endsWith(".ppm")) {
-                        try {
-                            System.out.println("Indique la calidad de compresión a usar (del 1 al 10)");
-                            ctrlProcesos.setCalidadJPEG(scanner.nextInt() * 10);
-                            fileComp = ctrlProcesos.comprimirArchivo(fileInCompDesc);
-                            if (fileComp != null)
-                                System.out.println("El archivo " + fileInCompDesc.getName() + " se ha comprimido correctamente!\n");
-                        }
-                        catch (Exception e) {}
+                        System.out.println("Indique la calidad de compresión a usar (del 1 al 10)");
+                        ctrlProcesos.setCalidadJPEG(scanner.nextInt() * 10);
+                        fileComp = ctrlProcesos.comprimirArchivo(fileInCompDesc);
+                        if (fileComp != null)
+                            System.out.println("El archivo " + fileInCompDesc.getName() + " se ha comprimido correctamente!\n");
+
                     }
-                    try {
-                        fileOutCompDesc = ctrlProcesos.descomprimirArchivo(fileComp);
-                        if(fileOutCompDesc != null) System.out.println("El archivo "+fileComp.getName()+" se ha descomprimido correctamente!\n");
-                    }
-                    catch (Exception e) {}
+                    fileOutCompDesc = ctrlProcesos.descomprimirArchivo(fileComp);
+                    if(fileOutCompDesc != null) System.out.println("El archivo "+fileComp.getName()+" se ha descomprimido correctamente!\n");
                     break;
                 case "salir":
+                case "4":
                     b = false;
                     break;
                 default:
