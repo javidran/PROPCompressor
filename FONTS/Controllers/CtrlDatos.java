96d144e024440919798ea1fd6f833cbfb2d8ca0e
sheidavanesa <sheidaat99@gmail.com>
2019-11-09 09:39:32 +0100

    Ini ControlDatos

--- FONTS/Controllers/CtrlDatos.java
+++ FONTS/Controllers/CtrlDatos.java
@@ -2,6 +2,7 @@
 package Controllers;
 
 import DomainLayer.Algoritmos.Algoritmos;
+import DomainLayer.Proceso.DatosProceso;
 
 import java.io.File;
 
@@ -15,10 +16,36 @@ public class CtrlDatos {
 
         return instance;
     }
+    public byte[] leerArchivo (String path) {
+        byte[] ret = new byte[0];
+        return ret;
+    }
+    
+    public void guardaArchivo (byte[] Data, String path, boolean b) {
+    }
+
+    public void actualizaEstadistica(DatosProceso dp, Algoritmos alg, boolean esCompresion) {}
+
+    public void obtenerDatosPrincipales(Algoritmos alg, boolean esCompresion) {}
+
+    public int getNumeroElementos(Algoritmos alg, boolean esCompresion) {
+        return 0;
+    }
+
+    public int getTiempoMedio(Algoritmos alg, boolean esCompresion) {
+        return 0;
+    }
+
+    public int getPorcentajeAhorradoMedio(Algoritmos alg, boolean esCompresion) {
+        return 0;
+    }
+
+    //public HashMap<int, long[3]> getDatosEstadistica(Algoritmos alg, boolean esCompresion) {  }
 
     public File estadisticasGlobales(Algoritmos algoritmo, boolean esCompresion) {
         File file = new File(System.getProperty("user.dir") + "/resources/estadistica_" + (esCompresion? "1":"0") + "_" + algoritmo);
         //System.out.println(file.getAbsolutePath() + "       " + file.exists());
         return file;
     }
+
 }
