e69d8434eeb959a45c97dc1cea09c4134cc116e6
sheidavanesa <sheidaat99@gmail.com>
2019-11-09 10:40:08 +0100

    Ctrl Datos

--- FONTS/Controllers/CtrlDatos.java
+++ FONTS/Controllers/CtrlDatos.java
@@ -1,10 +1,10 @@
-// Creado por Jan Escorza Fuertes
+// Creado por Sheida Vanesa Alfaro Taco
 package Controllers;
 
 import DomainLayer.Algoritmos.Algoritmos;
 import DomainLayer.Proceso.DatosProceso;
 
-import java.io.File;
+import java.io.*;
 
 public class CtrlDatos {
     private static CtrlDatos instance = null;
@@ -17,14 +17,37 @@ public class CtrlDatos {
         return instance;
     }
     public byte[] leerArchivo (String path) {
-        byte[] ret = new byte[0];
-        return ret;
-    }
-    
-    public void guardaArchivo (byte[] Data, String path, boolean b) {
+        File archivo = new File(path);
+        byte[] archivoBytes = new byte[(int) archivo.length()];
+        try
+        {
+            FileInputStream fis = new FileInputStream(archivo);
+            int numBytes = fis.read(archivoBytes);
+            fis.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el archivo.");
+        } catch (IOException e) {
+            System.out.println("No se ha podido leer el archivo.");
+        }
+        return archivoBytes;
     }
 
-    public void actualizaEstadistica(DatosProceso dp, Algoritmos alg, boolean esCompresion) {}
+    public void guardaArchivo (byte[] Data, String path, boolean sobreescribir) {
+        File archivo = new File(path);
+        try {
+            FileOutputStream fos = new FileOutputStream(archivo);
+            fos.write(Data);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el archivo.");
+        } catch (IOException e) {
+            System.out.println("No se ha podido escribir en el archivo.");
+        }
+    }
+
+    public void actualizaEstadistica(DatosProceso dp, Algoritmos alg, boolean esCompresion) {
+
+    }
 
     public void obtenerDatosPrincipales(Algoritmos alg, boolean esCompresion) {}
 
---
+++ FONTS/DataLayer/GestorEstadisticas.java
@@ -0,0 +1,61 @@
+package DataLayer;
+
+import DomainLayer.Algoritmos.Algoritmos;
+import DomainLayer.Proceso.DatosProceso;
+
+import java.io.*;
+
+public class GestorEstadisticas {
+    private static GestorEstadisticas instance = null;
+
+    public static GestorEstadisticas getInstance()
+    {
+        if (instance == null)
+            instance = new GestorEstadisticas();
+
+        return instance;
+    }
+
+    protected void guardaDatos(DatosProceso dp, Algoritmos alg, boolean esCompresion) throws IOException {
+        File estadistica = new File( System.getProperty("user.dir") +"/resources/estadistica_"+(esCompresion? "1":"0")+"_"+alg+".txt");
+        long a = dp.getTiempo();
+        long b = dp.getOldSize();
+        long c = dp.getNewSize();
+        long d = dp.diffTam();
+        long numDatos = 1;
+        long tiempoMedio = a;
+        long AvgMedio = d;
+
+        if (estadistica.exists()) {
+            RandomAccessFile archivo = new RandomAccessFile(estadistica, "rw");
+            archivo.seek(0);
+            String buff = archivo.readLine();
+            String[] parts = buff.split(" ");
+            numDatos = (Long.parseLong(parts[0]) + 1);
+            long aux = ((Long.parseLong( parts[1])*(numDatos-1))/numDatos + a/numDatos);
+            tiempoMedio = aux;
+            aux =  ((Long.parseLong(parts[2])*(numDatos-1))/numDatos + d/numDatos);
+            AvgMedio = aux;
+            archivo.seek(0);
+            archivo.writeBytes(numDatos +" "+ tiempoMedio +" "+ AvgMedio);
+            archivo.close();
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(estadistica, true));
+            CharSequence cs = (a + " " + b + " " + c + " " + d);
+            bw.newLine();
+            bw.append(cs);
+            bw.close();
+        } else {
+            estadistica.createNewFile();
+            BufferedWriter bw = new BufferedWriter(new FileWriter(estadistica));
+            bw.write(numDatos + " " + tiempoMedio + " " + AvgMedio);
+            bw.newLine();
+            bw.close();
+
+            BufferedWriter bw2 = new BufferedWriter(new FileWriter(estadistica, true));
+            CharSequence cs = (a + " " + b + " " + c + " " + d);
+            bw2.append(cs);
+            bw2.close();
+        }
+    }
+}
