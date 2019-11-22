2185001ba3e164286a5faf14805e3d65c72d75ed
sheidavanesa <sheidaat99@gmail.com>
2019-11-22 11:47:35 +0100

    Interface

---
+++ FONTS/PresentationLayer/MyInterface.java
@@ -0,0 +1,13 @@
+package PresentationLayer;
+
+import javax.swing.*;
+
+public class MyInterface extends JFrame {
+    private JPanel panel;
+
+    public MyInterface () {
+        super ("Ejemplo interfaz.");
+        setContentPane(panel);
+    }
+}
+
