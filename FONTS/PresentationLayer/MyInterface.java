6321e3d3f1493a3cd680af5a91e8a84a6a6c9254
Javier <javidran80@gmail.com>
2019-11-24 12:54:37 +0100

    Explorar modificado

--- FONTS/PresentationLayer/MyInterface.java
+++ FONTS/PresentationLayer/MyInterface.java
@@ -1,6 +1,13 @@
 package PresentationLayer;
 
+import Controllers.CtrlProcesos;
+import Exceptions.FormatoErroneoException;
+
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 public class MyInterface extends JFrame {
     private JPanel panel;
@@ -9,10 +16,56 @@ public class MyInterface extends JFrame {
     private JTextField textField1;
     private JButton variableButton;
     private JButton estadísticasButton;
+    private JButton explorarButton;
 
     public MyInterface () {
         super ("Ejemplo interfaz.");
         setContentPane(panel);
+        explorarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                textField1.setText(seleccionDeArchivo());
+            }
+        });
+    }
+
+    private String seleccionDeArchivo() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+        FileFilter fileFilter = new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                if(file.isDirectory()) return true;
+
+                String path = file.getName();
+                String[] splitP = path.split("\\.");
+                String type = splitP[splitP.length-1];
+
+                switch (type) {
+                    case "txt":
+                    case "ppm":
+                    case "imgc":
+                    case "lzss":
+                    case "lz78":
+                    case "lzw":
+                    case "comp":
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+            @Override
+            public String getDescription() {
+                return "*.txt,*.ppm,*.imgc,*.lzss,*.lz78,*.lzw,*.comp";
+            }
+        };
+
+        chooser.setFileFilter(fileFilter);
+        chooser.showOpenDialog(null);
+        return chooser.getSelectedFile().getAbsolutePath();
     }
 }
 
