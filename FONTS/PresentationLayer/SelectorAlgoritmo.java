1bfeecffbebc5757b992ae82de3a4a09623cf240
Jan <jan.escorza99@gmail.com>
2019-11-26 12:22:52 +0100

    Gestion de sobreescribir un archivo

--- FONTS/PresentationLayer/SelectorAlgoritmo.java
+++ FONTS/PresentationLayer/SelectorAlgoritmo.java
@@ -33,7 +33,23 @@ public class SelectorAlgoritmo extends JDialog {
         procesarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                System.exit(0);
+                //System.exit(0);
+                boolean existe = (new File(pathSalida.getText())).exists();
+                if (existe) {
+                    int respuesta = JOptionPane.showConfirmDialog(null, "El fichero resultante del proceso sobrescribirá uno ya existente, ¿desea sobrescribirlo?", "Sobrescribir",
+                            JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+                    if (respuesta == JOptionPane.NO_OPTION) {
+                        //Se cierra el cuadro de dialogo y se le da al usuario la posibilidad de escoger un nuevo path
+                    } else if (respuesta == JOptionPane.YES_OPTION) {
+                        //PROCESAR EL FICHERO
+//LLAMADA A PROCESAR EL FICHERO --> AQUI
+                    } else if (respuesta == JOptionPane.CLOSED_OPTION) {
+                        //Si se cierra con la cruz, se procede igual que si ha dicho que no quiere sobrescribir
+                    }
+                } else {
+                    // File or directory does not exist
+                    System.exit(0);
+                }
             }
         });
         cancelarButton.addActionListener(new ActionListener() {
@@ -92,7 +108,7 @@ public class SelectorAlgoritmo extends JDialog {
             String path = file.getAbsolutePath();
             String[] splitP = path.split("\\.");
             String type = splitP[splitP.length-1];
-            if (type.equalsIgnoreCase("extensionchunga")) {
+            if (type.equalsIgnoreCase("txt")) {
                 // filename is OK as-is
             } else {
                 path = path.replace(type, "extensionchunga"); // ALTERNATIVELY: remove the extension (if any) and replace it with ".xml"
