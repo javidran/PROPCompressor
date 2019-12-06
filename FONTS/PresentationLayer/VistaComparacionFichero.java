045102b1bf93b4db80f0b826ceda6c5bfcad140e
Javier <javidran80@gmail.com>
2019-12-06 21:15:47 +0100

    Fichero eliminado y datosProceso bonicos

--- FONTS/PresentationLayer/VistaComparacionFichero.java
+++ FONTS/PresentationLayer/VistaComparacionFichero.java
@@ -4,6 +4,7 @@ import DomainLayer.Proceso.DatosProceso;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.DecimalFormat;
 
 public class VistaComparacionFichero extends JDialog {
     public JTable original;
@@ -31,13 +32,14 @@ public class VistaComparacionFichero extends JDialog {
         resultadoPanel.getVerticalScrollBar().setUnitIncrement(16);
         resultadoPanel.getHorizontalScrollBar().setUnitIncrement(16);
 
-        tiempoC.setText(Long.toString(dp[0].getTiempo()));
+        DecimalFormat df = new DecimalFormat("#.####");
+        tiempoC.setText(df.format((double)dp[0].getTiempo() / 1000000000.0));
         OldSizeC.setText(Long.toString(dp[0].getOldSize()));
         NewSizeC.setText(Long.toString(dp[0].getNewSize()));
         DiffSizeC.setText(Long.toString(dp[0].getDiffSize()));
         RatioC.setText(Double.toString(dp[0].getDiffSizePercentage()));
 
-        tiempoD.setText(Long.toString(dp[1].getTiempo()));
+        tiempoD.setText(df.format((double)dp[1].getTiempo() / 1000000000.0));
         OldSizeD.setText(Long.toString(dp[1].getOldSize()));
         NewSizeD.setText(Long.toString(dp[1].getNewSize()));
         DiffSizeD.setText(Long.toString(dp[1].getDiffSize()));
