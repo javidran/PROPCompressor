a03b778f8ea4dea9cf42af9d730d887e6623e5ff
sheidavanesa <sheidaat99@gmail.com>
2019-12-06 12:34:02 +0100

    Nombrecito

--- FONTS/PresentationLayer/VistaComparacionFichero.java
+++ FONTS/PresentationLayer/VistaComparacionFichero.java
@@ -11,13 +11,33 @@ public class VistaComparacionFichero extends JDialog {
     private JButton finalizarButton;
     private JScrollPane scrollPanel;
     private JPanel panel;
+    private JLabel tiempoC;
+    private JLabel OldSizeC;
+    private JLabel NewSizeC;
+    private JLabel DiffSizeC;
+    private JLabel RatioC;
+    private JLabel tiempoD;
+    private JLabel OldSizeD;
+    private JLabel NewSizeD;
+    private JLabel DiffSizeD;
+    private JLabel RatioD;
 
     public VistaComparacionFichero(Frame owner, DatosProceso[] dp){
         super (owner, "Proceso completado",true);
         setContentPane(panel);
         scrollPanel.getVerticalScrollBar().setUnitIncrement(16);
+        tiempoC.setText(Long.toString(dp[0].getTiempo()));
+        OldSizeC.setText(Long.toString(dp[0].getOldSize()));
+        NewSizeC.setText(Long.toString(dp[0].getNewSize()));
+        DiffSizeC.setText(Long.toString(dp[0].getDiffSize()));
+        RatioC.setText(Double.toString(dp[0].getDiffSizePercentage()));
 
-        //TODO mostrar cosas de datos proceso
+        tiempoD.setText(Long.toString(dp[1].getTiempo()));
+        OldSizeD.setText(Long.toString(dp[1].getOldSize()));
+        NewSizeD.setText(Long.toString(dp[1].getNewSize()));
+        DiffSizeD.setText(Long.toString(dp[1].getDiffSize()));
+        RatioD.setText(Double.toString(dp[1].getDiffSizePercentage()));
+
 
 
         finalizarButton.addActionListener(e -> dispose());
